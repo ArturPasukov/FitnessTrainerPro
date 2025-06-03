a30159f7a03598a77932d3c41fef28f60ab35710
furpas <arturpasuk@yandex.ru>
2025-06-03 10:30:20 +0300

    feat: Add initial WorkoutProgramManagementWindow (XAML and empty CS)

---
+++ src/FitnessTrainerPro.UI/WorkoutProgramManagementWindow.xaml.cs
@@ -0,0 +1,26 @@
+using System.Windows;
+// using FitnessTrainerPro.Core.Models;
+// using FitnessTrainerPro.Data;
+// using System.Linq;
+// using Microsoft.EntityFrameworkCore;
+
+namespace FitnessTrainerPro.UI
+{
+    public partial class WorkoutProgramManagementWindow : Window
+    {
+        public WorkoutProgramManagementWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // LoadPrograms(); // Загрузка программ при открытии
+        }
+
+        // private void LoadPrograms() { /* ... */ }
+        // private void AddProgramButton_Click(object sender, RoutedEventArgs e) { /* ... */ }
+        // private void EditProgramButton_Click(object sender, RoutedEventArgs e) { /* ... */ }
+        // private void DeleteProgramButton_Click(object sender, RoutedEventArgs e) { /* ... */ }
+    }
+}
