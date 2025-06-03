f7e33d2606316c763b079ec305c0089d80989c0a
furpas <arturpasuk@yandex.ru>
2025-06-03 10:58:47 +0300

    feat: Update WorkoutProgramEditWindow UI and basic logic for ProgramExercises list

--- src/FitnessTrainerPro.UI/WorkoutProgramEditWindow.xaml.cs
+++ src/FitnessTrainerPro.UI/WorkoutProgramEditWindow.xaml.cs
@@ -1,22 +1,25 @@
 using System.Windows;
 using FitnessTrainerPro.Core.Models;
-// using System.Collections.Generic; // Для ProgramExercises
-// using System.Linq;
+using System.Collections.Generic; // Для List
+using System.Collections.ObjectModel; // Для ObservableCollection
+using System.Linq;
+using FitnessTrainerPro.Data; // Для доступа к FitnessDbContext, если понадобится (например, для выбора упражнений)
 
 namespace FitnessTrainerPro.UI
 {
     public partial class WorkoutProgramEditWindow : Window
     {
         public WorkoutProgram CurrentProgram { get; private set; }
-        // public List<ProgramExercise> ProgramExercisesList { get; set; } // Для хранения упражнений программы
+        // Используем ObservableCollection для автоматического обновления ListView
+        public ObservableCollection<ProgramExercise> ProgramExercisesList { get; set; }
 
         // Конструктор для новой программы
         public WorkoutProgramEditWindow()
         {
             InitializeComponent();
             CurrentProgram = new WorkoutProgram();
-            // ProgramExercisesList = new List<ProgramExercise>();
-            // TODO: Bind ProgramExercisesList to a ListView for exercises in program
+            ProgramExercisesList = new ObservableCollection<ProgramExercise>();
+            ProgramExercisesListView.ItemsSource = ProgramExercisesList; // Привязываем к ListView
         }
 
         // Конструктор для редактирования существующей программы
@@ -24,13 +27,23 @@ namespace FitnessTrainerPro.UI
         {
             InitializeComponent();
             CurrentProgram = programToEdit;
-            // ProgramExercisesList = programToEdit.ProgramExercises.ToList(); // Копируем список
-
+            
+            // Заполняем основные поля программы
             ProgramNameTextBox.Text = CurrentProgram.Name;
             ProgramFocusTextBox.Text = CurrentProgram.Focus;
             ProgramDescriptionTextBox.Text = CurrentProgram.Description;
-            
-            // TODO: Populate ListView for exercises in program from ProgramExercisesList
+
+            // Заполняем список упражнений программы
+            // Важно, чтобы programToEdit.ProgramExercises был загружен (через Include в вызывающем коде)
+            if (CurrentProgram.ProgramExercises != null)
+            {
+                ProgramExercisesList = new ObservableCollection<ProgramExercise>(CurrentProgram.ProgramExercises.OrderBy(pe => pe.OrderInProgram));
+            }
+            else
+            {
+                ProgramExercisesList = new ObservableCollection<ProgramExercise>();
+            }
+            ProgramExercisesListView.ItemsSource = ProgramExercisesList; // Привязываем к ListView
         }
 
         private void OkButton_Click(object sender, RoutedEventArgs e)
@@ -45,8 +58,18 @@ namespace FitnessTrainerPro.UI
                 return;
             }
 
-            // TODO: Сохранить ProgramExercisesList обратно в CurrentProgram.ProgramExercises
-            // CurrentProgram.ProgramExercises = new List<ProgramExercise>(ProgramExercisesList);
+            // Обновляем коллекцию ProgramExercises в CurrentProgram на основе ProgramExercisesList
+            // Это важно, чтобы EF Core мог отследить изменения в коллекции
+            // Сначала очистим старые, если они есть (более сложная логика синхронизации потребуется для Attach/Update)
+            CurrentProgram.ProgramExercises.Clear(); 
+            foreach (var pe in ProgramExercisesList)
+            {
+                CurrentProgram.ProgramExercises.Add(pe);
+            }
+            // Более продвинутый способ - синхронизировать коллекции, удаляя, обновляя и добавляя элементы.
+            // Пока что просто заменяем коллекцию. Это сработает для новых программ.
+            // Для редактирования существующих программ с сохранением через тот же DbContext,
+            // EF Core должен отследить изменения в коллекции CurrentProgram.ProgramExercises, если она была загружена с Include.
 
             this.DialogResult = true;
         }
@@ -56,6 +79,49 @@ namespace FitnessTrainerPro.UI
             this.DialogResult = false;
         }
 
-        // TODO: Методы для добавления, редактирования, удаления упражнений из ProgramExercisesList
+        // --- Обработчики для кнопок управления упражнениями в программе ---
+
+        private void AddProgramExerciseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Открыть окно выбора упражнения (SelectExerciseWindow)
+            // TODO: После выбора упражнения, открыть окно для ввода сетов, репов и т.д. (ProgramExerciseParamsWindow)
+            // TODO: Добавить новый ProgramExercise в ProgramExercisesList
+            MessageBox.Show("Функционал 'Добавить упражнение в программу' еще не реализован.", "В разработке");
+        }
+
+        private void EditProgramExerciseButton_Click(object sender, RoutedEventArgs e)
+        {
+            ProgramExercise? selectedProgramExercise = ProgramExercisesListView.SelectedItem as ProgramExercise;
+            if (selectedProgramExercise == null)
+            {
+                MessageBox.Show("Пожалуйста, выберите упражнение из программы для редактирования.", "Внимание");
+                return;
+            }
+            // TODO: Открыть окно для редактирования параметров выбранного ProgramExercise (ProgramExerciseParamsWindow)
+            // TODO: Обновить selectedProgramExercise в ProgramExercisesList
+            MessageBox.Show($"Редактирование упражнения '{selectedProgramExercise.Exercise?.Name}' еще не реализовано.", "В разработке");
+        }
+
+        private void DeleteProgramExerciseButton_Click(object sender, RoutedEventArgs e)
+        {
+            ProgramExercise? selectedProgramExercise = ProgramExercisesListView.SelectedItem as ProgramExercise;
+            if (selectedProgramExercise == null)
+            {
+                MessageBox.Show("Пожалуйста, выберите упражнение из программы для удаления.", "Внимание");
+                return;
+            }
+
+            MessageBoxResult result = MessageBox.Show($"Удалить упражнение '{selectedProgramExercise.Exercise?.Name}' из программы?",
+                                                     "Подтверждение удаления",
+                                                     MessageBoxButton.YesNo,
+                                                     MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                ProgramExercisesList.Remove(selectedProgramExercise);
+                // TODO: Подумать о том, как это отразится на CurrentProgram.ProgramExercises при сохранении.
+                // Если ProgramExercisesList - это та же коллекция, что и в CurrentProgram, то удаление здесь будет достаточным
+                // перед тем как EF Core сохранит изменения.
+            }
+        }
     }
 }
